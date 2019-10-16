02f10f8da119924caceb22d16f67cf3dd5c24707
xenia <adri.espanol@gmail.com>
2019-10-16 17:56:18 +0300

    [Feature] Login page [JS]

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import LoginPage from './pages/Login';
+
+import './styles/index.scss';
+
+function App() {
+  return (
+    <div className="app">
+      <LoginPage />
+    </div>
+  );
+}
+
+export default App;
